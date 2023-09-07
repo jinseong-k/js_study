597a8d644ddab1729af9c82383b8bf6da3bc9a3b
Jinseong <nadas9029@gmail.com>
2023-09-07 20:49:15 +0900

    calculator 생성 로직 함수분리 및 변수 명 수정

--- example2/Calculator.js
+++ example2/Calculator.js
@@ -1,206 +1,287 @@
 const OPS = ["+", "-", "*", "/"];
-const pad = document.getElementById("pad");
-const KEY_PAD_COUNT = 15;
+const OP_PAD_CHAR = ['C', '+', '-', '*', '/'];
+const OP_PAD_COUNT = 5;
+const NUM_PAD_CHAR = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '.', '0', '='];
+const NUM_PAD_COUNT = 12;
+const EMPTY_PAD_COUNT = 3;
+
+// 1. calculator error fix
+// 2. 일반 계산기처럼 동작하게 하기
+// 3. history: undo(실행취소)/redo(다시실행)
+// 4. history 저장하기/불러오기 (브라우저)
 
 class Calculator {
-
     constructor() {
 
     }
 
     _plusOp(form) {
-
+        let token = form.split('+');
+        return parseFloat(token[0]) + parseFloat(token[1]);
     }
 
     _minusOp(form) {
-
+        let token = form.split('-');
+        return parseFloat(token[0]) - parseFloat(token[1]);
     }
 
     _multiOp(form) {
-
+        let token = form.split('*');
+        return parseFloat(token[0]) * parseFloat(token[1]);
     }
 
     _divOp(form) {
-
+        let token = form.split('/');
+        return parseFloat(token[0]) / parseFloat(token[1]);
     }
 
-    calculate(form) {
+    calculate(form, minusFlag) {
         let resultValue;
-        let token;
+        let index;
 
         if (form.includes('+')) {
-            token = form.split('+');
-            resultValue = parseFloat(token[0]) + parseFloat(token[1]);
+            index = form.indexOf('+');
+            return this.calculate(form.slice(0, index)) + this.calculate(form.slice(index+1));
         } else if (form.includes('-')) {
-            token = form.split('-');
-            resultValue = parseFloat(token[0]) - parseFloat(token[1]);
+            index = form.indexOf('-');
+            return this.calculate(form.slice(0, index)) - (-1 * (this.calculate(form.slice(index+1))));
         } else if (form.includes('*')) {
-            token = form.split('*');
-            resultValue = parseFloat(token[0]) * parseFloat(token[1]);
+            index = form.indexOf('*');
+            return this.calculate(form.slice(0, index)) * this.calculate(form.slice(index+1));
         } else if (form.includes('/')) {
-            token = form.split('/');
-            resultValue = parseFloat(token[0]) / parseFloat(token[1]);
+            index = form.indexOf('/');
+            return this.calculate(form.slice(0, index)) / this.calculate(form.slice(index+1));
         } else {
-            resultValue = inputText.value;
+            return parseFloat(form);
         }
-        return resultValue;
     }
 }
+
+const func = () => {
+    const createCaclElement = () => {
+        const calcElement = document.createElement(`div`);
+        calcElement.className = 'calc';
+
+        return calcElement;
+    };
+    const createTextArea = () => {
+        const textArea = document.createElement('div');
+        textArea.className = `text-area`;
+
+        return textArea;
+    };
+};
 
 class View {
-    _name;
+    _textArea;
     _inputText;
     _resultText;
+    _pad;
+    _calculator;
 
-    constructor(name) {
-        this._name = name;
+    constructor(calculator) {
+        this._calculator = calculator;
     }
 
-    createHtml(_name) {
-        const numPadChar = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '.', '0', '='];
-        const opPadChar = ['C', '+', '-', '*', '/'];
-    
+    _createCalcElement() {
         const calcElement = document.createElement(`div`);
-        calcElement.id = `calc${this._name}`;
         calcElement.className = 'calc';
-    
-        const container3 = document.createElement('div');
-        container3.className = `container3`;
-    
+
+        return calcElement;
+    }
+
+    _createTextArea() {
+        const textArea = document.createElement('div');
+        textArea.className = `text-area`;
+
+        return textArea;
+    }
+
+    _createResultArea() {
         const resultTextArea = document.createElement('p');
         resultTextArea.className = `resultText`;
-        resultTextArea.id = `resultText${this._name}`;
         resultTextArea.style.backgroundColor = 'white';
-        container3.appendChild(resultTextArea);
-    
+
+        return resultTextArea;
+    }
+
+    _createInputArea() {
         const input = document.createElement('input');
         input.className = 'inputText';
-        input.type='text';
-        input.id = `inputText${this._name}`;
-        container3.appendChild(input);
+        input.type = 'text';
 
+        input.addEventListener('keydown', (event) => {
+            this._inputTextKeyPress(event);
+        });
+
+        return input;
+    }
+
+    _inputTextKeyPress(event) {
+        const INSERTABLE_KEYS = ['Enter', 'Space', 'Backspace',
+            '+', '-', '/', '*', "(", ")", ".",
+            '0', '1', '2', '3', '4',
+            '5', '6', '7', '8', "9"];
+        const valid = INSERTABLE_KEYS.includes(event.key);
+
+        if (!valid) {
+            event.preventDefault();
+            return;
+        }
+
+        if (event.key === 'Enter') {
+            this._equalButtonClickHandler();
+            event.preventDefault();
+            return;
+        }
+    }
+
+    _createPad() {
         const pad = document.createElement('div');
         pad.className = 'pad';
-        pad.id = `pad${this._name}`;
 
-        const container1 = document.createElement('div');
-        container1.className = 'container1';
-        pad.appendChild(container1);
+        pad.addEventListener('click', this._clickEventListener);
 
-        const numPadDiv = [];
-        const numButton = [];
-        for (let i = 0; i < KEY_PAD_COUNT; i++) {
-            numPadDiv[i] = document.createElement('div')
-            numPadDiv[i].className = 'item';
-            if (i > 2) {
-                numButton[i] = document.createElement('button');
-                numButton[i].className = 'myButton';
-                numButton[i].id = `button${i}`;
-                numButton[i].innerText = numPadChar[i-3];
-                numButton[i].id = `button${numPadChar[i-3]}`;
-                numButton[i].value = `${numPadChar[i-3]}`;
-                numPadDiv[i].appendChild(numButton[i]);
-            }
-            container1.appendChild(numPadDiv[i]);
+        return pad;
+    }
+
+    _clickEventListener = (event) => {
+        const { value } = event.target;
+
+        if (!value) {
+            return;
         }
 
-        const opDiv = [];
-        const opButton = [];
-        const container2 = document.createElement('div');
-        container2.className = 'container2';
-
-        for (let i = 0; i < 5; i++) {
-            opDiv[i] = document.createElement('div');
-            opDiv[i].className = 'item';
-            opButton[i] = document.createElement('button');
-            opButton[i].className = 'myButton';
-            opButton[i].innerText = opPadChar[i];
-            opButton[i].id = `button${opPadChar[i]}`;
-            opButton[i].value = `${opPadChar[i]}`;
-
-            opDiv[i].appendChild(opButton[i]);
-            container2.appendChild(opDiv[i]);
+        if (OPS.includes(value)) {
+            this._operatorButtonClickHandler(value);
+        } else if (value === "C") {
+            this._clearButtonClickHandler();
+        } else if (value === "=") {
+            this._equalButtonClickHandler();
+        } else {
+            this._buttonClickHandler(value);
         }
-        pad.appendChild(container2);
+    }
 
-        calcElement.appendChild(container3);
-        calcElement.appendChild(pad);
+    _operatorButtonClickHandler(value) {
+        if (this._inputText.value.length <= 0) return;
+        this._inputText.value = `${this._inputText.value}${value}`;
+    }
 
-        document.body.appendChild(calcElement);
-        this._inputText = document.getElementById(`inputText${this._name}`);
-        this._inputText.addEventListener('keydown', (event) => {
-            inputTextKeyPress(event, this._inputText, this._resultText);
-        });
-        this._resultText = document.getElementById(`resultText${this._name}`);
+    _clearButtonClickHandler() {
+        this._inputText.value = '';
+        this._resultText.innerText = '';
+    }
 
-        let elementPad = document.getElementById(`pad${this._name}`);
-        elementPad.addEventListener('click', event => {
-            const {value} = event.target;
+    _equalButtonClickHandler() {
+        let result = this._calculator.calculate(this._inputText.value);
+        this._resultText.innerText = isNaN(result) ? "incorrect Formular" : result;
 
-            if (!value) {
-                return;
-            }
+        this._inputText.value = '';
+    }
 
-            if (OPS.includes(value)) {
-                operatorButtonClickHandler(value, this._inputText);
-            } else if (value === "C") {
-                clearButtonClickHandler(this._inputText, this._resultText);
-            } else if (value === "=") {
-                equalButtonClickHandler(this._inputText, this._resultText);
-            } else {
-                buttonClickHandler(value, this._inputText);
-            }
-        });
+    _buttonClickHandler(value) {
+        this._inputText.value = `${this._inputText.value}${value}`;
+    }
+
+    _createNumPad() {
+        const numPad = document.createElement('div');
+        numPad.className = 'num-pad';
+
+        return numPad;
+    }
+
+    _createEmptyPadDiv() {
+        const emptyPadDiv = document.createElement('div');
+        emptyPadDiv.className = 'item';
+
+        return emptyPadDiv;
+    }
+
+    _createNumPadDiv(index) {
+        const numPadDiv = document.createElement('div')
+        numPadDiv.className = 'item';
+        const numButton = document.createElement('button');
+        numButton.className = 'myButton';
+        numButton.innerText = NUM_PAD_CHAR[index];
+        numButton.value = `${NUM_PAD_CHAR[index]}`;
+        numPadDiv.appendChild(numButton);
+
+        return numPadDiv;
+    }
+
+    _createOpPad() {
+        const opPad = document.createElement('div');
+        opPad.className = 'op-pad';
+
+        return opPad;
+    }
+
+    _createOpPadDiv(index) {
+        const opDiv = document.createElement('div');
+        opDiv.className = 'item';
+        const opButton = document.createElement('button');
+        opButton.className = 'myButton';
+        opButton.innerText = OP_PAD_CHAR[index];
+        opButton.value = `${OP_PAD_CHAR[index]}`;
+
+        opDiv.appendChild(opButton);
+
+        return opDiv
+    }
+
+    _createTextAreaPart() {
+        this._textArea = this._createTextArea();
+
+        this._resultText = this._createResultArea();
+        this._textArea.appendChild(this._resultText);
+
+        this._inputText = this._createInputArea();
+        this._textArea.appendChild(this._inputText);
+
+    }
+
+    _createPadPart() {
+        this._pad = this._createPad();
+
+        const numPad = this._createNumPad();
+        this._pad.appendChild(numPad);
+
+        for (let i = 0; i < EMPTY_PAD_COUNT; i++) {
+            const emptyPadDiv = this._createEmptyPadDiv();
+            numPad.appendChild(emptyPadDiv);
+        }
+
+        for (let i = 0; i < NUM_PAD_COUNT; i++) {
+            const numPadDiv = this._createNumPadDiv(i);
+            numPad.appendChild(numPadDiv);
+        }
+
+        const opPad = this._createOpPad();
+
+        for (let i = 0; i < OP_PAD_COUNT; i++) {
+            const opDiv = this._createOpPadDiv(i);
+            opPad.appendChild(opDiv);
+        }
+        this._pad.appendChild(opPad);
+
+    }
+
+    _createCalcPart() {
+        this._calcElement = this._createCalcElement();
+        this._calcElement.appendChild(this._textArea);
+        this._calcElement.appendChild(this._pad);
+    }
+
+    createHtml(id) {
+        this._createTextAreaPart();
+        this._createPadPart();
+        this._createCalcPart();
+
+        document.getElementById(id).appendChild(this._calcElement);
     }
 }
-
-const view = new View("First");
-view.createHtml();
-
-const INSERTABLE_KEYS = ['Enter', 'Space', 'Backspace',
-                        '+', '-', '/', '*', "(", ")", ".",
-                        '0', '1', '2', '3', '4',
-                        '5', '6', '7', '8', "9"];
-const checkInputKey = key => INSERTABLE_KEYS.includes(key);
 
 const calculator = new Calculator();
-
-function buttonClickHandler(value, inputText) {
-    inputText.value = `${inputText.value}${value}`;
-}
-
-function equalButtonClickHandler(inputText, resultText) {
-    resultText.innerText = calculator.calculate(inputText.value);
-    inputText.value = '';
-}
-
-function operatorButtonClickHandler(value, inputText) {
-    if (inputText.value.length <= 0) return;
-    inputText.value = `${inputText.value}${value}`;
-}
-
-function clearButtonClickHandler(inputText, resultText) {
-    inputText.value = '';
-    resultText.innerText = '';
-}
-
-function inputTextKeyPress(event, inputText, resultText) {
-    const valid = checkInputKey(event.key);
-
-    if (!valid) {
-        event.preventDefault();
-        return;
-    }
-
-    if (event.key === 'Enter') {
-        equalButtonClickHandler(inputText, resultText);
-        event.preventDefault();
-        return;
-    }
-}
-
-// 3.
-//   - 계산기와 UI 로직 분리
-// Mouse/Keyboard -> Key Checker(filtering) -> Calculator -> UI
-
-// inputText.addEventListener('keydown', event => inputTextKeyPress(event));
+const view = new View(calculator);
+view.createHtml("first")
