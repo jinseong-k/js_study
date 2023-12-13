2bfda7a510af9a82e160fff04398d4785faeba74
yonggoo-noh <yonggoo.noh@navercorp.com>
2023-12-13 22:58:42 +0900

    store 구현하여 TextAreaPart 리팩토링

--- example3/calculator/src/app/Calculator.js
+++ example3/calculator/src/app/Calculator.js
@@ -1,64 +1,16 @@
 
 // Store..
 export class Calculator {
-    _resultValue;
-    _curOperator;
-    _initValue;
-
-    constructor(initValue) {
-        this._resultValue = 0;
-        this._curOperator = null;
-        this._initValue = initValue;
-    }
-
-    /**
-     * JSDoc (JavaScript Documentation)
-     * @param {"+"|"-"|"*"|"/"} op
-     */
-    set curOperator(op) {
-        this._curOperator = op;
-    }
-
-    get initValue() {
-        return this._initValue;
-    }
-
-    setValue(value) {
-        this._resultValue = value;
-        this._curOperator = null;
-    }
-
-    calculate(number, op) {
-        this._resultValue = this._calculate(this._resultValue, this._curOperator, number);
-        this._curOperator = op;
-
-        return this._resultValue;
-    }
-
-    _calculate(prev, op, number) {
-
+    static calculate(result, input, op) {
         switch(op) {
             case '+':
-                return parseFloat(prev)+parseFloat(number);
+                return parseFloat(result) + parseFloat(input);
             case '-':
-                return parseFloat(prev)-parseFloat(number);
+                return parseFloat(result) - parseFloat(input);
             case '*':
-                return parseFloat(prev)*parseFloat(number);
+                return parseFloat(result) * parseFloat(input);
             case '/':
-                return parseFloat(prev)/parseFloat(number);
-            case '=':
-                const value = this._calculate(prev, this._curOperator, number);
-
-                this._curOperator = null;
-
-                return value;
-            default:
-                return number;
+                return parseFloat(result) / parseFloat(input);
         }
-    }
-
-    clearCalculator() {
-        this._resultValue = 0;
-        this._curOperator = null;
     }
 }
