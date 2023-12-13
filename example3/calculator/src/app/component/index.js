a652485e254b8fd3f63e14e21190c301f282ef2f
yonggoo-noh <yonggoo.noh@navercorp.com>
2023-12-13 22:08:08 +0900

    Context 주입은 최상위 한 곳에서 이루어질 수 있도록 수정

--- example3/calculator/src/app/component/index.js
+++ example3/calculator/src/app/component/index.js
@@ -1,4 +1,3 @@
-export * from "./Panel";
 export * from "./ModalItem";
 export * from "./PadPart";
 export * from "./TextAreaPart"
--- example3/calculator/src/app/page.js
+++ example3/calculator/src/app/page.js
@@ -1,17 +1,27 @@
 "use client"
+import {useMemo, useState} from "react";
 import {Calculator} from './Calculator';
 import {History} from './History';
-import {CalculatorContext} from "./context";
-import {Panel, ModalItem} from "@/app/component/";
+import {CalculatorContext, InputContext, ResultContext} from "./context";
+import {ModalItem, TextAreaPart, PadPart} from "@/app/component/";
 
 export default function Home() {
     const calculator = new Calculator(0);
     const history = new History();
+    const [result, setResult] = useState(0);
+    const [input, setInput] = useState("");
+    const resultValue = useMemo(() => ({result, setResult}), [result, setResult]);
+    const inputValue = useMemo(() => ({input, setInput}), [input, setInput]);
 
     return (
         <CalculatorContext.Provider value={calculator}>
-            <Panel />
-            <ModalItem />
+            <ResultContext.Provider value={resultValue}>
+                <InputContext.Provider value={inputValue}>
+                    <TextAreaPart />
+                    <PadPart />
+                    <ModalItem />
+                </InputContext.Provider>
+            </ResultContext.Provider>
         </CalculatorContext.Provider>
     )
 }
