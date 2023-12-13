9e83ff2e9886c5b6d72073972350a52774af30d5
yonggoo-noh <yonggoo.noh@navercorp.com>
2023-12-13 22:28:57 +0900

    StoreContext로 랩핑하여 전달

--- example3/calculator/src/app/hooks/index.js
+++ example3/calculator/src/app/hooks/index.js
@@ -1,8 +1,6 @@
 import {useContext} from "react";
-import {CalculatorContext, InputContext, ResultContext} from "../context";
+import {CalculatorContext, StoreContext} from "../context";
 
 export const useCalculatorContext = () => useContext(CalculatorContext);
 
-export const useInputContext = () => useContext(InputContext);
-
-export const useResultContext = () => useContext(ResultContext);
+export const useStoreContext = () => useContext(StoreContext);
