98276b22b45ea6bda551b36f396adfdd21e03f66
yonggoo-noh <yonggoo.noh@navercorp.com>
2023-12-13 22:03:06 +0900

    잘못 선언된 context default value 수정 (타입 오류)

--- example3/calculator/src/app/context/TextContext.js
+++ example3/calculator/src/app/context/TextContext.js
@@ -1,4 +1,4 @@
 import { createContext } from 'react';
 
-export const ResultContext = createContext(0);
-export const InputContext = createContext(0);
+export const ResultContext = createContext(null);
+export const InputContext = createContext(null);
